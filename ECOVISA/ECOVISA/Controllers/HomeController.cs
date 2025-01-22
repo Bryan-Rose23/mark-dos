6b97d17b49037b8842495f3cfaa3b01e0cd182c8
Bryan-Rose23 <56421942+Bryan-Rose23@users.noreply.github.com>
2025-01-21 21:58:05 -0600

    Initialize Repository

---
+++ ECOVISA/ECOVISA/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CapaNegocio;
+
+namespace ECOVISA.Controllers
+{
+    [OutputCache(NoStore = true, Duration = 0, VaryByParam = "*")]
+    public class HomeController : Controller
+    {
+        public ActionResult Inicio()
+        {
+            if (Session["TARGET_PAGE"] == null) 
+            {
+                Session["TARGET_PAGE"] = "/Home/Index";
+            }
+            clsNegocioUsuario cnUsuario = new clsNegocioUsuario();
+            cnUsuario.ceUsuario = (CapaEntidades.clsEntidadUsuario)Session["SesionUsuario"];
+            TempData["NombreUsuarioSession"] = cnUsuario.ceUsuario.NombreUsuario;
+            return View();
+        }
+
+        [HttpGet]
+        public PartialViewResult Index()
+        {
+            Session["TARGET_PAGE"] = "/Home/Index";
+            return PartialView();
+        }
+
+        [HttpGet]
+        public PartialViewResult About()
+        {
+            ViewBag.Message = "Your contact page.";
+            Session["TARGET_PAGE"] = "/Home/About";
+            return PartialView();
+        }
+
+        [HttpGet]
+        public PartialViewResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            Session["TARGET_PAGE"] = "/Home/Contact";
+            return PartialView();
+        }
+
+        public ActionResult CerrarSesion()
+        {
+            Session["SesionUsuario"] = null;
+            Session["TARGET_PAGE"] = null;
+            return RedirectToAction("Login", "Acceso");
+        }
+    }
+}
